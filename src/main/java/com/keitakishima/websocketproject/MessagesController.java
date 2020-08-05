9054d02b3973dc5d721528e455a04ed835b7d6e7
Kai Hackemesser <kai.hackemesser@gmail.com>
2020-08-05 18:26:15 +1200

    fixed project.

--- src/main/java/com/keitakishima/websocketproject/MessagesController.java
+++ src/main/java/com/keitakishima/websocketproject/MessagesController.java
@@ -2,9 +2,11 @@ package com.keitakishima.websocketproject;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
 import java.util.Date;
 
+@Controller
 public class MessagesController {
 
     //handle route /chat with send
--- src/main/java/com/keitakishima/websocketproject/WebConfig.java
+++ src/main/java/com/keitakishima/websocketproject/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
+                .addResourceLocations("classpath:/META-INF/resources/webjars/").resourceChain(false) ;
     }
 }
